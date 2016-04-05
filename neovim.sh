805e339fd9705a286be89f24a2a05a09202f1b13
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-04 23:44:25 -0700

    Create neovim.sh

---
+++ neovim.sh
@@ -0,0 +1,5 @@
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+sudo apt-get install python-dev python-pip python3-dev python3-pip
