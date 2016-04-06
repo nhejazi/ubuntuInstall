d21dd7de3cd8cb4e23bff2fdc1fc8e3225f7f78d
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-06 13:32:22 -0700

    Update _getBrew.sh

--- _getBrew.sh
+++ _getBrew.sh
@@ -9,6 +9,5 @@ brew doctor
 brew tap homebrew/science
 
 # get core Linuxbrew tools
-brew install wget git coreutils curl
-brew install zmq czmq libzmq3
+brew install wget coreutils zmq czmq libzmq3
 brew install libsvg libxml2 gdal geos boost
