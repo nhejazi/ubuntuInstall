6d0a55695d206fea561f688c0a79a66f50e6ac15
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-06 13:36:25 -0700

    configure vim setup

--- _getMyVim.sh
+++ _getMyVim.sh
@@ -4,16 +4,12 @@
 brew update
 brew install vim
 brew install neovim/neovim/neovim
-brew linkapps macvim
 
 # setup preferred configuration for Vim and MacVim
 if [ -e ~/.vim ]; then 
   rm -rf ~/.vim; 
 fi
 
-if [ -e ~/.vimrc]; then 
-  rm -f ~/.vimrc; 
-fi
-
 git clone git@github.com:nhejazi/myvimconfig.git ~/.vim
 sh ~/.vim/_setup.sh
+cd ~/.vim; sh _update.sh
