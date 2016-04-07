041b2dbed65f3dfcc60be9345c8f0707abcfe2f6
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-07 10:33:38 -0700

    Delete _getNeovim.sh

--- _getNeovim.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-apt-get install software-properties-common
-add-apt-repository ppa:neovim-ppa/unstable
-apt-get update
-apt-get install neovim
-apt-get install python-dev python-pip python3-dev python3-pip
