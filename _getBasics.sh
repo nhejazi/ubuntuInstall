0895049f2d2925c2d697b9ea27244c9d9b4185a2
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-07 10:23:06 -0700

    reformat

--- _getBasics.sh
+++ _getBasics.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-apt-get install build-essential curl git m4 python-setuptools ruby texinfo libbz2-dev 
-apt-get install libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+apt-get install build-essential software-properties-common python-setuptools curl git ruby m4
+apt-get install libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
