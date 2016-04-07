154c63b1d5b5c6b8c89674d9cdd18b6eec4ee61b
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-07 10:13:34 -0700

    remove sudo

--- _getNoBrew.sh
+++ _getNoBrew.sh
@@ -1,22 +1,24 @@
+#!/usr/bin/bash
+
 # julia install
-sudo add-apt-repository ppa:staticfloat/juliareleases
-sudo add-apt-repository ppa:staticfloat/julia-deps
-sudo apt-get update
-sudo apt-get install julia
+add-apt-repository ppa:staticfloat/juliareleases
+add-apt-repository ppa:staticfloat/julia-deps
+apt-get update
+apt-get install julia
 
 
 # scala install
 wget www.scala-lang.org/files/archive/scala-2.11.7.deb
-sudo dpkg -i scala-2.11.7.deb
+dpkg -i scala-2.11.7.deb
 
 # sbt installation
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
-sudo apt-get update
-sudo apt-get install sbt
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+apt-get update
+apt-get install sbt
 
 # java install
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
+apt-get install python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java8-installer
