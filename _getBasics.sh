d80694f91ab9a60e038b501b191e28fda50fc605
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-09 15:23:59 -0700

    add docker reqs

--- _getBasics.sh
+++ _getBasics.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
-apt-get install build-essential software-properties-common python-setuptools curl git ruby m4
-apt-get install libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+apt-get install build-essential software-properties-common python-setuptools 
+apt-get install curl git ruby m4 apt-transport-https ca-certificates libbz2-dev
+apt-get install libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
