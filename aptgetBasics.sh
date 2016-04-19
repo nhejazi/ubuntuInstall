bb3e47c27052f0906d6b481e4b2a13732a8e43aa
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-18 18:37:01 -0700

    Update aptgetBasics.sh

--- aptgetBasics.sh
+++ aptgetBasics.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # get core tools with apt-get
+apt-get install build-essential software-properties-common python-setuptools 
+apt-get install curl git ruby m4 apt-transport-https ca-certificates libbz2-dev
+apt-get install libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+
 apt-get install wget libzmq5-dev libczmq-dev
 apt-get install libboost-all-dev libgeos-dev
 apt-get install libgdal-dev libxml2-dev
+
 
 # setup dotfiles
 git clone https://github.com/nhejazi/mydotfiles.git ~/.dotfiles
