4d772cc1f092863a705b80a3eb8ad50923ac0d42
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-18 18:35:42 -0700

    Update aptgetBasics.sh

--- aptgetBasics.sh
+++ aptgetBasics.sh
@@ -4,3 +4,7 @@
 apt-get install wget libzmq5-dev libczmq-dev
 apt-get install libboost-all-dev libgeos-dev
 apt-get install libgdal-dev libxml2-dev
+
+# setup dotfiles
+git clone https://github.com/nhejazi/mydotfiles.git ~/.dotfiles
+sh ~/.dotfiles/_setup.sh
