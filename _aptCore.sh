afc2a2886f1cef3b85999347469792419f92b763
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-26 13:21:51 -0700

    add python-software-properties + reorganize

--- _aptCore.sh
+++ _aptCore.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # get core tools with apt-get
-apt-get install build-essential software-properties-common python-setuptools 
-apt-get install curl git ruby m4 apt-transport-https ca-certificates libbz2-dev
-apt-get install libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev;
-apt-get -y install libcurl4-gnutls-dev libxml2-dev libssl-dev
-
-apt-get install wget libzmq5-dev libczmq-dev
-apt-get install libboost-all-dev libgeos-dev
-apt-get install libgdal-dev libxml2-dev
+apt-get install build-essential software-properties-common 
+apt-get install python-setuptools python-software-properties
+apt-get install wget curl git ruby m4 apt-transport-https ca-certificates
+apt-get install libcurl4-openssl-dev libexpat-dev libncurses-dev libbz2-dev
+apt-get install libcurl4-gnutls-dev libxml2-dev libssl-dev libboost-all-dev
+apt-get install libzmq5-dev libczmq-dev libgeos-dev libgdal-dev zlib1g-dev
 
 
 # setup dotfiles
