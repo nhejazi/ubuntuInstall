60641d9d4c9dd2791eab97826acc63473d53c789
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-26 14:21:27 -0700

    remove linuxbrew files

--- _linuxbrew/_getBasics.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-get install build-essential software-properties-common python-setuptools 
-apt-get install curl git ruby m4 apt-transport-https ca-certificates libbz2-dev
-apt-get install libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
--- _linuxbrew/_initBrew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# get Linuxbrew utility
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/linuxbrew/go/install)"
-
-# setup dotfiles
-git clone https://github.com/nhejazi/mydotfiles.git ~/.dotfiles
-sh ~/.dotfiles/_setup.sh
