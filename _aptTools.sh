2ef0526771de6b579ea4354fd2cb5d7284618f6b
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-26 17:33:38 -0700

    Update _aptTools.sh

--- _aptTools.sh
+++ _aptTools.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# pandoc (markup tool) install
+# Pandoc (markup tool) install
 wget http://github.com/jgm/pandoc/releases/download/1.17.0.2/pandoc-1.17.0.2-1-amd64.deb
 dpkg -i pandoc-1.17.0.2-1-amd64.deb
 
-# atom (editor) install
+# Atom (editor) install
 wget http://github.com/atom/atom/releases/download/v1.7.2/atom-amd64.deb
 dpkg -i atom-amd64.deb
+
+# fix installation issues arising from Atom depedencies
+apt-get install -f
