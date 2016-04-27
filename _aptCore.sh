5fca1a03b5a814dd3d9339b053bcd3d4c3bd0f2f
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-27 15:02:43 -0700

    add libzmq3 + libzmq3-dev for IRkernel

--- _aptCore.sh
+++ _aptCore.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # get core tools with apt-get
-apt-get install build-essential software-properties-common 
-apt-get install python-setuptools python-software-properties
-apt-get install wget curl git ruby m4 apt-transport-https ca-certificates
+apt-get install build-essential software-properties-common wget curl
+apt-get install python-setuptools python-software-properties git ruby
+apt-get install m4 apt-transport-https ca-certificates libzmq3 libzmq3-dev
 apt-get install libcurl4-openssl-dev libexpat-dev libncurses-dev libbz2-dev
 apt-get install libcurl4-gnutls-dev libxml2-dev libssl-dev libboost-all-dev
 apt-get install libzmq5-dev libczmq-dev libgeos-dev libgdal-dev zlib1g-dev
