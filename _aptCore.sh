4cfad93ce6aab61ca376fa5174ca212968a9ddf6
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-28 11:41:40 -0700

    add package ruby-dev

--- _aptCore.sh
+++ _aptCore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # get core tools with apt-get
-apt-get install build-essential software-properties-common wget curl
-apt-get install python-setuptools python-software-properties git ruby
+apt-get install build-essential software-properties-common wget curl git
+apt-get install python-setuptools python-software-properties ruby ruby-dev
 apt-get install m4 apt-transport-https ca-certificates libzmq3 libzmq3-dev
 apt-get install libcurl4-openssl-dev libexpat-dev libncurses-dev libbz2-dev
 apt-get install libcurl4-gnutls-dev libxml2-dev libssl-dev libboost-all-dev
