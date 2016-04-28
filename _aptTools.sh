c39ff577b9a2370a9482d57e329075047a68ddbf
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-28 11:54:39 -0700

    update jekyll install to ruby2.0

--- _aptTools.sh
+++ _aptTools.sh
@@ -8,8 +8,8 @@ dpkg -i pandoc-1.17.0.2-1-amd64.deb
 wget http://github.com/atom/atom/releases/download/v1.7.2/atom-amd64.deb
 dpkg -i atom-amd64.deb
 
-# Jekyll (for blog posts) install via Ruby
-gem install jekyll
+# Jekyll (for blog posts) install via Ruby (requires 2.0)
+gem2.0 install jekyll
 
 # fix installation issues arising from Atom depedencies
 apt-get install -f
