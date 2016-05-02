bb334eaa713ffab5ab3b0745aaa67e280ecf5d33
Nima Hejazi <nima.hejazi7@gmail.com>
2016-05-02 12:28:15 -0700

    add neovim setup

--- _aptVim.sh
+++ _aptVim.sh
@@ -13,4 +13,7 @@ fi
 
 git clone https://github.com/nhejazi/myvimconfig.git ~/.vim
 sh ~/.vim/_setup.sh
-cd ~/.vim; sh _update.sh
+cd ~/.vim; sh _update.sh; cd
+
+git clone https://github.com/nhejazi/myneovimconfig.git ~/.config/nvim
+sh ~/.config/nvim/_setup.sh
