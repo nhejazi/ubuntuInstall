2044e14d055f00f2d98ad53ad4d42ad762800727
Nima Hejazi <nima.hejazi7@gmail.com>
2016-06-11 18:27:18 -0700

    add sudo catch

--- _aptVim.sh
+++ _aptVim.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
 # get Vim and NeoVim
 add-apt-repository ppa:neovim-ppa/unstable
 apt-get update
