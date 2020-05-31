8e47413f50085e495c41869599aa2636ed1c7093
Nima Hejazi <nh@nimahejazi.org>
2020-05-31 11:29:15 -0700

    fix hugo msg; update pandoc; rm hyper

--- tools.sh
+++ tools.sh
@@ -6,20 +6,14 @@ if [[ "$EUID" -ne 0 ]]; then
 fi
 
 
-# Hyper (terminal emulator)
-#echo "Installing Hyper v2.1.0-canary.2 \nCHECK this is the most recent stable release."
-#wget http://github.com/zeit/hyper/releases/download/2.1.0-canary.2/hyper_2.1.0-canary.2_amd64.deb
-#dpkg -i hyper_2.1.0-canary.2_amd64.deb
-
-
 # pandoc (markup conversion tool)
-echo "Installing Pandoc v.2.9.1.1 \n PLEASE CHECK that this is the most recent stable release."
-wget http://github.com/jgm/pandoc/releases/download/2.9.1.1/pandoc-2.9.1.1-1-amd64.deb
-dpkg -i pandoc-2.9.1.1-1-amd64.deb
+echo "Installing Pandoc v.2.9.2.1 \n PLEASE CHECK that this is the most recent stable release."
+wget http://github.com/jgm/pandoc/releases/download/2.9.2.1/pandoc-2.9.2.1-1-amd64.deb
+dpkg -i pandoc-2.9.2.1-1-amd64.deb
 
 
 # Hugo (modern static site generator)
-echo "Installing Hugo v0.64.1 \nCHECK this is the most recent stable release."
+echo "Installing Hugo v0.72.0 \nCHECK this is the most recent stable release."
 wget https://github.com/gohugoio/hugo/releases/download/v0.72.0/hugo_extended_0.72.0_Linux-64bit.deb
 dpkg -i hugo_extended_0.72.0_Linux-64bit.deb
 
