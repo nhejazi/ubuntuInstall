151a3269d39857254bab79200435613ecfe52eb4
Nima Hejazi <nh@nimahejazi.org>
2021-10-11 12:48:26 -0400

    remove miniconda for pyenv

--- nosudo.sh
+++ nosudo.sh
@@ -18,8 +18,3 @@ sh ~/.vim/_setup.sh
 
 # install pyenv for project environment management
 curl https://pyenv.run | bash
-
-
-# conda for latest Python 3
-curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
