6446ba7e4deffe9b0ed23be1263644818db6726b
Nima Hejazi <nh@nimahejazi.org>
2021-10-11 23:10:05 -0400

    fixes for python installation

--- python.sh
+++ python.sh
@@ -1,5 +1,5 @@
-# install and setup Python via pyenv
-pyenv install python 3.9.7
+# install Python and set up base environment via pyenv
+pyenv install 3.9.7
 pyenv virtualenv 3.9.7 base_env
 pyenv global base_env
 
@@ -9,8 +9,8 @@ python -m pip install --upgrade pip
 # nicer displays of system information
 python -m pip install glances bpytop
 
-# fancy manpages
+# fancy manpages and how-tos
 python -m pip tldr howdoi
 
-# modern R repl
-python -m pip install radian
+# modern REPLs for Python and R
+python -m pip install ipython radian
